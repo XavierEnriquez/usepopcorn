6cf8639e3e5ffac4beead6b14127034a6d029820
Xavi Enriquez <113799913+XavierEnriquez@users.noreply.github.com>
2024-10-29 12:07:47 -0500

    Update App.js
    Add watched movies in local storage feature

--- src/App.js
+++ src/App.js
@@ -33,10 +33,13 @@ function SearchResultsBlock({ isLoading, error, movies, handleSelectMovie }) {
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+  const [watched, setWatched] = useState(function () {
+    const storedMovies = localStorage.getItem("watched");
+    return JSON.parse(storedMovies);
+  });
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -53,6 +56,13 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imbdID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
